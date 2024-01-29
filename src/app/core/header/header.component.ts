7cb190e2d9fcf0a032448b7ff7f102e8ff9d17aa
George Samourgkanidis <giorgossamou1@gmail.com>
2024-01-29 20:05:06 +0200

    Implement Responsive Header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,13 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { Router, RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { Route } from '../../models/routes';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule, MatToolbarModule, MatButtonModule],
+  imports: [NgFor, RouterModule, MatToolbarModule, MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  styleUrl: './header.component.scss'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  pages: Route[] = [
+    {
+      label: 'Adopt Kittens',
+      link: '/adopt-kittens'
+    },
+    {
+      label: 'Kitty Stories',
+      link: '/kitty-stories'
+    },
+    {
+      label: 'About Us',
+      link: '/about-us'
+    },
+    {
+      label: 'Blog',
+      link: '/blog'
+    },
+    {
+      label: 'Contact',
+      link: '/contact'
+    }
+  ];
+
+  constructor(private router: Router) {}
+
+  navigateTo(link: string) {
+    this.router.navigateByUrl(link);
+  }
+}
